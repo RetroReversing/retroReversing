21fe314ad063258545584899e267a51330b8ca1d
RetroGameDeveloper <40120498+RetroGameDeveloper@users.noreply.github.com>
2019-03-04 21:29:29 +0000

    Update generateTOC.js

--- public/js/generateTOC.js
+++ public/js/generateTOC.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
     $( ".post h1" ).each(function( index ) {
         console.error("H1:",index);
-        $(".post-directory dl").append("<dt><a class=\"jumper\" href=#" +
+        $(".post-directory ul").append("<li><a class=\"jumper\" href=#" +
                 $(this).attr("id")
                 + ">"
                 + $(this).text()
-                + "</a></dt>");
+                + "</a></li>");
 
         var children = $(this).nextUntil("h1", "h2,h3,h4,h5,h6")
 
         children.each(function( index ) {
-            $(".post-directory dl").append("<dd><a class=\"jumper--child\" href=#" +
+            $(".post-directory ul").append("<li><a class=\"jumper--child\" href=#" +
                     $(this).attr("id")
                     + ">"
                     + "<span class=\"jumper--"+$(this).prop("tagName")+"\">" + $(this).text()
-                    + "</span></a></dd>");
+                    + "</span></a></li>");
         });
     });
 
