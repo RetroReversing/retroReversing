ca7072cd5311f044ca4c82bae6853f6a49c98f53
RetroGameDeveloper <40120498+RetroGameDeveloper@users.noreply.github.com>
2019-03-04 20:52:36 +0000

    Update generateTOC.js

--- public/js/generateTOC.js
+++ public/js/generateTOC.js
@@ -1,24 +1,22 @@
+$(document).ready(function(){
+    $( ".post h1" ).each(function( index ) {
+        console.error("H1:",index);
+        $(".post-directory dl").append("<dt><a class=\"jumper\" hre=#" +
+                $(this).attr("id")
+                + ">"
+                + $(this).text()
+                + "</a></dt>");
 
+        var children = $(this).nextUntil("h2", "h3", "h4","h5","h6")
 
-    $(document).ready(function(){
-        $( ".post h1" ).each(function( index ) {
-            console.error("H1:",index);
-            $(".post-directory dl").append("<dt><a class=\"jumper\" hre=#" +
+        children.each(function( index ) {
+            $(".post-directory dl").append("<dd><a class=\"jumper\" href=#" +
                     $(this).attr("id")
                     + ">"
-                    + $(this).text()
-                    + "</a></dt>");
-
-            var children = $(this).nextUntil("h2", "h3", "h4","h5","h6")
-
-            children.each(function( index ) {
-                $(".post-directory dl").append("<dd><a class=\"jumper\" hre=#" +
-                        $(this).attr("id")
-                        + ">"
-                        + "&nbsp;&nbsp;- " + $(this).text()
-                        + "</a></dd>");
-            });
+                    + "&nbsp;&nbsp;- " + $(this).text()
+                    + "</a></dd>");
         });
-
     });
+
+});
 
