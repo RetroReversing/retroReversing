ee05bf5d00b07e6d81fd5d8938b4a6f6720750c4
RetroGameDeveloper <40120498+RetroGameDeveloper@users.noreply.github.com>
2019-11-08 21:52:19 +0000

    Update generateTOC.js

--- public/js/generateTOC.js
+++ public/js/generateTOC.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
             $(".post-directory dl").append("<dt><a class=\"jumper--child\" href=#" +
                     $(this).attr("id")
                     + ">"
-                    + "<li class=\"jumper--"+$(this).prop("tagName")+"\"> * " + $(this).text()
+                    + "<li class=\"jumper--"+$(this).prop("tagName")+"\"> " + $(this).text()
                     + "</li></a></dt>");
         });
     });
