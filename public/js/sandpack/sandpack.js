2f23db2cb4a65562f4427f1c5c131910cb28e27d
RetroGameDeveloper <40120498+RetroGameDeveloper@users.noreply.github.com>
2025-05-18 13:08:24 +0100

    Update sandpack.js

--- public/js/sandpack/sandpack.js
+++ public/js/sandpack/sandpack.js
@@ -122,6 +122,7 @@ class RRSandpack extends HTMLElement {
         theme: nightOwl,
         files,
         options: {
+          externalResources: ["https://cdn.tailwindcss.com"]
           showTabs: showTabsAttr,
           showLineNumbers: true,
           showConsoleButton: true,
